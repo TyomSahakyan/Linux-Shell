44ef52c3f1912613e35f8e04594b59ffa40fc0f8
TyomSahakyan <96411291+TyomSahakyan@users.noreply.github.com>
2022-03-31 20:22:35 +0400

    Update Shell.h

--- Shell.h
+++ Shell.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef MY_SHELL
+#define MY_SHELL
 
 #include <stdio.h>
 #include <sys/wait.h>
@@ -8,11 +9,16 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
+
 #define BUFFER_SIZE 1024
 #define TOK_BUFFER_SIZE 100
 #define TOK_DELIM " "
 
 char* read_line();
 char** split_line(char*);
+void check_pipes(char**, char**);
+int find_pipe(char*, char**);
 int execute(char**);
 void inf_loop();
+
+#endif
